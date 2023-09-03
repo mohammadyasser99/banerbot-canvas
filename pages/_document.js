2c44d9799258ab88de28e37ecb82e18c673f1d0c
mohammadyasser99 <131922865+mohammadyasser99@users.noreply.github.com>
2023-09-03 23:31:04 +0300

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,14 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Link from 'next/link'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+            <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
+        <Link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet" />
+        </Head>
       <body>
         <Main />
         <NextScript />
